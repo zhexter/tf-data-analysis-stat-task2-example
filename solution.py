3c55dc88d954bce6a5e5e2dce39c56bcb85c4168
Eugenia <zhexter@gmail.com>
2023-04-04 11:01:42 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,12 +4,13 @@ import numpy as np
 from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 12162367 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    x = (x * 2) / (50 * 50)
     alpha = 1 - p
     loc = x.mean()
     scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
