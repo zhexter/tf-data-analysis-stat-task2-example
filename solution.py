0fe4cc6ff0a571be5dc67bdc60f3c32704d2e99f
Eugenia <zhexter@gmail.com>
2023-04-05 09:28:32 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,10 +10,8 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    #loc = x.mean()
-        
-    x = (x * 2) / (50 * 50)
     alpha = 1 - p
-    #np.quantile(-(expon.pdf(x) - 1/2), alpha / 2)
-    #scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return np.quantile(-(expon.pdf(x) - 1/2), 1 - alpha / 2) / min(x), np.quantile(-(expon.pdf(x) - 1/2), alpha / 2) / min(x)
+    n = len(x)
+    left = (-min(-x) - 1 / 2) / (50**2 / 2)
+    right = (-np.log(alpha) / n -min(-x) - 1 / 2) / (50**2 / 2)
+    return left, right
